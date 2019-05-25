261162af91b3dcaef15d2645a9a1ceb9d24437bc
Rodrigo M <rodrigo.medina.neri@gmail.com>
2019-05-24 20:58:11 -0500

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Roeeyn on 5/24/19.
+//  Copyright © 2019 Roeeyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
